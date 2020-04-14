916ab5f5f58f14d33b59d4dcbbf2b28e02e6c589
Laurent Milne <laurent.milne@bottlerocketstudios.com>
2020-04-13 23:11:57 -0500

    added all dom elements

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Switch from './Switch';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   const [light, setLight] = useState(true)
+
+   return (
+      <div id="wall" className={light ? "on" : null}>
+         <Switch onClick={() => setLight(!light)} />
+         <div id="bulb" className={light ? "on" : "off"}></div>
+         <div id="light" className={light ? "lit" : null}></div>
+         <div id="lamp" className={light ? "on" : null}></div>
+      </div>
+
+   );
 }
 
 export default App;
